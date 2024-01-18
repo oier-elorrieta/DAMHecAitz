cee2e4c41bdd4ceff41337282f227e39f63f249f
Oier P <infopalacios@elorrieta-errekamari.com>
2024-01-18 10:54:26 +0100

    Bigarren commit. Peli klasea eginda. Erabiltzaile hasita

--- src/Modelo/Pelikula.java
+++ src/Modelo/Pelikula.java
@@ -1,6 +1,26 @@
 package Modelo;
 
 public class Pelikula {
+	private int adina;
+	private String mezua;
+	public Pelikula(int adina, String mezua) {
+		this.adina = adina;
+		this.mezua = mezua;
+	}
+	public int getAdina() {
+		return adina;
+	}
+	public void setAdina(int adina) {
+		this.adina = adina;
+	}
+	public String getMezua() {
+		return mezua;
+	}
+	public void setMezua(String mezua) {
+		this.mezua = mezua;
+	}
+	
+	
 	
 
 }
