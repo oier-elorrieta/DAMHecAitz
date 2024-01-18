ea8b8dfbfa43f5e923b05d8da2ee90a33d7e0fcb
Héctor Martinez <hector.martineznu@elorreita-errekamari.com>
2024-01-18 12:32:15 +0100

    Aldaketa Hector main

--- src/Controlador/Erronka3APPT2.java
+++ src/Controlador/Erronka3APPT2.java
@@ -3,7 +3,8 @@ package Controlador;
 public class Erronka3APPT2 {
 
 	public static void main(String[] args) {
-		
+		System.out.println();
+		//Hector
 
 	}
 
--- src/Modelo/Pelikula.java
+++ src/Modelo/Pelikula.java
@@ -20,7 +20,7 @@ public class Pelikula {
 		this.mezua = mezua;
 	}
 	
-	djhjd
+	
 	
 
 }
